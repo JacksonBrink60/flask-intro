940aebb7155337a1a86a7173de360558e61c53b7
Jackson Brink <brinkjackson101@gmail.com.com>
2025-05-14 09:10:46 -0400

    Added dogs

--- app.py
+++ app.py
@@ -21,7 +21,7 @@
 
 # Create an instance of the Flask app
 from flask import Flask, render_template, request # render_template loads HTML from /templates
-import datetime
+import datetime, requests
 
 
 app = Flask(__name__)
@@ -60,6 +60,42 @@ def math():
         num2 = int(request.form.get("num2"))
         return render_template("math_results.html", num1=num1, num2=num2)
     return render_template("math.html")
+
+@app.route("/catfact")
+def catfact():
+    response = requests.get("https://catfact.ninja/fact")
+    if response.status_code == 200:  # we successfully got a response
+        data = response.json()
+        fact = data["fact"]
+    else:
+        fact = "Could not fetch a cat fact right now. Try again later"
+
+    picresp = requests.get("https://cataas.com/cat?json=true")
+    if picresp.status_code == 200:  # we successfully got a response
+        picdata = picresp.json()
+        pic = picdata["url"]
+    else:
+        pic = "/images/404.png"
+
+    return render_template("catfact.html", cat_fact=fact, pic=pic)
+
+@app.route("/dog", methods=["GET", "POST"])
+def dog():
+    image_url = None #This will be the dog image
+    error = None #Holds an error message if something goes wrong
+
+    if request.method== "POST":
+        breed = request.form.get("breed").lower()
+        api_url = f"https://dog.ceo/api/breed/{breed}/images/random"
+        response = requests.get(api_url)
+
+        #check for valid response
+        if response.status_code == 200 and response.json().get("status") == "success":
+            image_url = response.json()["message"]
+        else:
+            error = f"Could not find breed \"{breed}\". Try another!"
+
+    return render_template("dog.html", image_url=image_url, error=error, breed=breed)
 
 if __name__ == "__main__":
     # debug = True enables automatic reload on changes and better error messages
